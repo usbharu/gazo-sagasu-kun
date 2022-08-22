6efc4112f2ab6bdb025f929c17d68a614abe5871
usbharu <64310155+usbharu@users.noreply.github.com>
2022-08-22 16:41:07 +0900

    テストデータが壊れていたのを修正

--- src/main/java/io/github/usbharu/imagesearch/domain/repository/ImageTagDaoJdbc.java
+++ src/main/java/io/github/usbharu/imagesearch/domain/repository/ImageTagDaoJdbc.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+@Deprecated
 @Repository("ImageTagDaoJdbc")
 public class ImageTagDaoJdbc implements ImageTagDao {
 

40a16aa9be3cce83013e473c1e282def12b1acca
usbharu <64310155+usbharu@users.noreply.github.com>
2022-09-03 08:05:41 +0900

    外部キー制約を組み込む

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,7 +2,8 @@ CREATE TABLE IF NOT EXISTS image(
                                     id      INTEGER PRIMARY KEY AUTOINCREMENT,
                                     name    TEXT        NOT NULL,
                                     path    TEXT UNIQUE NOT NULL,
-                                    groupId INTEGER     NOT NULL
+                                    groupId INTEGER     NOT NULL,
+                                    FOREIGN KEY (groupId) REFERENCES groupId(id)
 );
 CREATE TABLE IF NOT EXISTS tag
 (
@@ -13,10 +14,12 @@ CREATE TABLE IF NOT EXISTS image_tag
 (
     image_id INTEGER NOT NULL,
     tag_id   INTEGER NOT NULL,
-    UNIQUE (image_id, tag_id)
+    UNIQUE (image_id, tag_id),
+    FOREIGN KEY (image_id) REFERENCES image(id),
+    FOREIGN KEY (tag_id) REFERENCES tag(id)
 );
 CREATE TABLE IF NOT EXISTS groupId
 (
-    id   INTEGER PRIMARY KEY AUTOINCREMENT,
+    id   INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE ,
     name TEXT NOT NULL UNIQUE
 );

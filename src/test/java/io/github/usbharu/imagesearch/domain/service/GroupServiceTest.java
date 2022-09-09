75cca4d49ee6554e73bf159c0c10eed64d908585
usbharu <64310155+usbharu@users.noreply.github.com>
2022-09-09 14:07:49 +0900

    GroupServiceとImageServiceのテストを追加

---
+++ src/test/java/io/github/usbharu/imagesearch/domain/service/GroupServiceTest.java
@@ -0,0 +1,75 @@
+package io.github.usbharu.imagesearch.domain.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+import io.github.usbharu.imagesearch.domain.model.Group;
+import io.github.usbharu.imagesearch.domain.repository.GroupDao;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.core.env.Environment;
+
+
+class GroupServiceTest {
+
+  @Mock
+  Environment environment;
+
+  @Mock
+  GroupDao groupDao;
+
+  @InjectMocks
+  GroupService groupService;
+
+  @BeforeEach
+  void setUp() {
+    MockitoAnnotations.initMocks(this);
+  }
+
+  @Test
+  void validation_groupName_returnTrue() {
+    when(environment.getProperty(anyString())).thenReturn("test");
+
+    assertTrue(groupService.validation("test"));
+  }
+
+  @Test
+  void validation_illegalGroupName_returnFalse() {
+    when(environment.getProperty(anyString())).thenReturn(null);
+
+    assertFalse(groupService.validation("illegalTest"));
+  }
+
+  @Test
+  void validation_null_throwNullPointerException() {
+    assertThrows(NullPointerException.class, () -> groupService.validation(null));
+  }
+
+  @Test
+  void validation_empty_throwIllegalArgumentException() {
+    assertThrows(IllegalArgumentException.class, () -> groupService.validation(""));
+  }
+
+  @Test
+  void validation_blank_throwIllegalArgumentException() {
+    assertThrows(IllegalArgumentException.class, () -> groupService.validation(" "));
+  }
+
+  @Test
+  void getGroup_getGroup_returnAllGroups() {
+
+    when(groupDao.findAll()).thenReturn(List.of(new Group("default"),new Group("test1")));
+    assertEquals(2,groupService.getGroups().size());
+  }
+
+  @Test
+  void getGroupAndAll_getGroupAndAll_returnAllGroupAndAll() {
+    when(groupDao.findAll()).thenReturn(List.of(new Group("default"),new Group("test1")));
+    assertEquals(3,groupService.getGroupsAndAll().size());
+  }
+}
---
+++ src/test/java/io/github/usbharu/imagesearch/domain/service/ImageScannerTest.java
@@ -0,0 +1,65 @@
+package io.github.usbharu.imagesearch.domain.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import io.github.usbharu.imagesearch.domain.repository.GroupDao;
+import io.github.usbharu.imagesearch.domain.repository.custom.BulkDao;
+import io.github.usbharu.imagesearch.domain.service.scan.Scanner;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.core.io.ClassPathResource;
+
+class ImageScannerTest {
+
+  @Mock
+  BulkDao bulkDao;
+
+  @Mock
+  GroupDao groupDao;
+
+  @Mock
+  Scanner scanner;
+
+  @InjectMocks ImageScanner imageScanner;
+
+  @BeforeEach
+  void setUp() {
+    MockitoAnnotations.initMocks(this);
+  }
+
+  @Test
+  void startScan_scanAll_addAll() throws NoSuchFieldException, IllegalAccessException, IOException {
+//    when(imageScanner.getGroup()).thenReturn(Map.of("test","/testData/1/"));
+//    when(imageScanner.getDepth()).thenReturn(3);
+//    when(imageScanner.getFolder()).thenReturn("/testData/");
+    Field group = imageScanner.getClass().getDeclaredField("group");
+    group.trySetAccessible();
+    group.set(imageScanner, Map.of("test","/testData/1/"));
+
+    Field folder = imageScanner.getClass().getDeclaredField("folder");
+    folder.trySetAccessible();
+    folder.set(imageScanner, new ClassPathResource("/testData/").getFile().getAbsolutePath());
+
+    doNothing().when(bulkDao).insertSplit(any(),anyInt());
+    doNothing().when(bulkDao).delete();
+
+    when(scanner.isSupported(any())).thenReturn(true);
+
+    imageScanner.startScan();
+    verify(bulkDao,times(1)).insertSplit(any(),anyInt());
+  }
+}

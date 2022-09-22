8cbc5bfa78e6ab561f257cfe8a3933ad31d3493a
usbharu <64310155+usbharu@users.noreply.github.com>
2022-09-22 11:06:46 +0900

    テストケースを修正

--- src/test/java/io/github/usbharu/imagesearch/domain/service/ImageSearchTest.java
+++ src/test/java/io/github/usbharu/imagesearch/domain/service/ImageSearchTest.java
@@ -11,12 +11,14 @@ import io.github.usbharu.imagesearch.domain.model.custom.Images;
 import io.github.usbharu.imagesearch.domain.repository.TagDao;
 import io.github.usbharu.imagesearch.domain.repository.custom.DynamicSearchDao;
 import io.github.usbharu.imagesearch.domain.service.duplicate.DuplicateCheck;
+import io.github.usbharu.imagesearch.util.ImageFileNameUtil;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 
 
 class ImageSearchTest {
@@ -32,6 +34,8 @@ class ImageSearchTest {
 
   @InjectMocks ImageSearch imageSearch;
 
+  @Spy
+  ImageFileNameUtil imageFileNameUtil;
   @BeforeEach
   void setUp() {
     MockitoAnnotations.initMocks(this);
@@ -42,11 +46,11 @@ class ImageSearchTest {
     Images images = new Images(4);
     Tags tags = new Tags();
     tags.add(new Tag("--123456--"));
-    Image image1 = new Image("1.jpg","1.jpg");
-    Image image2 = new Image("2.jhg","2.jpg");
-    Image image3 = new Image("3.jpg","3.jpg");
+    Image image1 = new Image("123456_p1.jpg","123456_p1.jpg");
+    Image image2 = new Image("123456_p2.jhg","123456_p2.jpg");
+    Image image3 = new Image("123456_p3.jpg","123456_p3.jpg");
 
-    Image image4 = new Image("4.jpg","4.jpg");
+    Image image4 = new Image("123457_p4.jpg","123457_p4.jpg");
     image1.addMetadata(tags);
     image2.addMetadata(tags);
     image3.addMetadata(tags);
@@ -56,6 +60,7 @@ class ImageSearchTest {
     image4.addMetadata(tags2);
     images.addAll(List.of(image1,image2,image3,image4));
     Images result = imageSearch.mergeSequentialNumbers(images);
+    System.out.println("result = " + result);
     assertEquals(2,result.size());
   }
 }

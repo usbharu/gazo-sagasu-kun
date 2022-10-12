fc4f4a472f04186d1afd5169dc6473adcb604a2e
usbharu <64310155+usbharu@users.noreply.github.com>
2022-10-12 21:40:34 +0900

    テストを追加 おかしくなっていたところを修正

--- src/test/java/io/github/usbharu/imagesearch/util/ImageFileNameUtilTest.java
+++ src/test/java/io/github/usbharu/imagesearch/util/ImageFileNameUtilTest.java
@@ -1,17 +1,163 @@
 package io.github.usbharu.imagesearch.util;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class ImageFileNameUtilTest {
+
+  @ParameterizedTest
+  @ValueSource(strings = {"usbharu_dev-1571732741414334454.img1.jpg",
+      "usbharu_dev-1571732741414334454.img1.JPG", "usbharu_dev-1571732741414334454.img1.jpeg",
+      "usbharu_dev-1571732741414334454.img1.JPEG", "DO5vA2.jpg", "1dq7.jpg"})
+  void isJpg_JpgFile_returnTrue(String string) {
+    ImageFileNameUtil imageFileNameUtil = new ImageFileNameUtil();
+    assertTrue(imageFileNameUtil.isJpg(string));
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"usbharu_dev-1571732741414334454.img1.png",
+      "usbharu_dev-1571732741414334454.img1.PNG", "9065IRC0.png", "riK.png", "u0RYBn.webp",
+      "pb7.gif", "ak9CIdTjpg"})
+  void isJpg_NotJpgFile_returnFalse(String string) {
+    ImageFileNameUtil imageFileNameUtil = new ImageFileNameUtil();
+    assertFalse(imageFileNameUtil.isJpg(string));
+  }
+
+  @Test
+  void isJpg_Null_throwNullPointException() {
+    ImageFileNameUtil imageFileNameUtil = new ImageFileNameUtil();
+    assertThrows(NullPointerException.class, () -> imageFileNameUtil.isJpg(null));
+  }
+
+  @Test
+  void isJpg_Empty_throwIllegalArgumentException() {
+    ImageFileNameUtil imageFileNameUtil = new ImageFileNameUtil();
+    assertThrows(IllegalArgumentException.class, () -> imageFileNameUtil.isJpg(""));
+  }
+
+  @Test
+  void isJpg_Blank_throwIllegalArgumentException() {
+    ImageFileNameUtil imageFileNameUtil = new ImageFileNameUtil();
+    assertThrows(IllegalArgumentException.class, () -> imageFileNameUtil.isJpg(" "));
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"usbharu_dev-1571732741414334464-img1.png",
+      "usbharu_dev-1571732741414334464-img1.PNG", "0Wtx0Yc3.png", "qF00O.png"})
+  void isPng_PngFile_returnTrue(String string) {
+    ImageFileNameUtil imageFileNameUtil = new ImageFileNameUtil();
+    assertTrue(imageFileNameUtil.isPng(string));
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"usbharu_dev-1571732741414334464-img1.jpg",
+      "usbharu_dev-1571732741414334464-img1.JPG", "usbharu_dev-1571732741414334464-img1.JPEG",
+      "usbharu_dev-1571732741414334464-img1.jpeg", "l2dpng"})
+  void isPng_NotPngFile_returnFalse(String string) {
+    ImageFileNameUtil imageFileNameUtil = new ImageFileNameUtil();
+    assertFalse(imageFileNameUtil.isPng(string));
+  }
+
+  @Test
+  void isPng_Null_throwNullPointException() {
+    ImageFileNameUtil imageFileNameUtil = new ImageFileNameUtil();
+    assertThrows(NullPointerException.class, () -> imageFileNameUtil.isPng(null));
+  }
+
+  @Test
+  void isPng_Empty_throwIllegalArgumentException() {
+    ImageFileNameUtil imageFileNameUtil = new ImageFileNameUtil();
+    assertThrows(IllegalArgumentException.class, () -> imageFileNameUtil.isPng(""));
+  }
+
+  @Test
+  void isPng_Blank_throwIllegalArgumentException() {
+    ImageFileNameUtil imageFileNameUtil = new ImageFileNameUtil();
+    assertThrows(IllegalArgumentException.class, () -> imageFileNameUtil.isPng(" "));
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"535932_p0.jpg", "1_p1.jpg", "11111111111111_p2222222222.png"})
+  void isPixivTypeFileName_PixivFile_returnTrue(String string) {
+    ImageFileNameUtil imageFileNameUtil = new ImageFileNameUtil();
+    assertThat(imageFileNameUtil.isPixivTypeFileName(string)).isTrue();
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"usbharu_dev-1571732741414334464-img1.jpg", "1234567890_.jpg",
+      "65543345543_pa.png", "346899875544_p1", "4828229592p1.jpg"})
+  void isPixivTypeFileName_NotPixivFile_returnFalse(String string) {
+    ImageFileNameUtil imageFileNameUtil = new ImageFileNameUtil();
+    assertFalse(imageFileNameUtil.isPixivTypeFileName(string));
+  }
+
+  @Test
+  void isPixivTypeFileName_Empty_throwIllegalArgumentException() {
+    ImageFileNameUtil imageFileNameUtil = new ImageFileNameUtil();
+    assertThrows(IllegalArgumentException.class, () -> imageFileNameUtil.isPng(""));
+  }
+
+  @Test
+  void isPixivTypeFileName_Blank_throwIllegalArgumentException() {
+    ImageFileNameUtil imageFileNameUtil = new ImageFileNameUtil();
+    assertThrows(IllegalArgumentException.class, () -> imageFileNameUtil.isPng(" "));
+  }
+
+  @Test
+  void isPixivTypeFileName_Null_throwNullPointException() {
+    ImageFileNameUtil imageFileNameUtil = new ImageFileNameUtil();
+    assertThrows(NullPointerException.class, () -> imageFileNameUtil.isPixivTypeFileName(null));
+  }
+
+  @ParameterizedTest
+  @CsvSource({
+      "1234567_p0.png,1234567",
+      "516_p1.jpg,516"
+  })
+  void getPixivTypeFileBaseName_pixivFileName_returnBaseName(String string, String expected) {
+    ImageFileNameUtil imageFileNameUtil = new ImageFileNameUtil();
+    assertEquals(expected, imageFileNameUtil.getPixivTypeFileBaseName(string));
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"3ZcLIVoe.jpg", "WU9_p0.jpg"})
+  void getPixivTypeFileBaseName_NotPixivFileName_returnNull(String string) {
+    ImageFileNameUtil imageFileNameUtil = new ImageFileNameUtil();
+    assertNull(imageFileNameUtil.getPixivTypeFileBaseName(string));
+  }
+
+  @Test
+  void getPixivTypeFileBaseName_Null_throwNullPointException() {
+    ImageFileNameUtil imageFileNameUtil = new ImageFileNameUtil();
+    assertThrows(NullPointerException.class,
+        () -> imageFileNameUtil.getPixivTypeFileBaseName(null));
+  }
+
+  @Test
+  void getPixivTypeFileBaseName_Empty_throwIllegalArgumentException() {
+    ImageFileNameUtil imageFileNameUtil = new ImageFileNameUtil();
+    assertThrows(IllegalArgumentException.class,
+        () -> imageFileNameUtil.getPixivTypeFileBaseName(""));
+  }
+
+  @Test
+  void getPixivTypeFileBaseName_Blank_throwIllegalArgumentException() {
+    ImageFileNameUtil imageFileNameUtil = new ImageFileNameUtil();
+    assertThrows(IllegalArgumentException.class,
+        () -> imageFileNameUtil.getPixivTypeFileBaseName(" "));
+  }
 
   @Test
   void getTwitterFileUserName_TwitterFile_returnUserName() {
     ImageFileNameUtil imageFileNameUtil = new ImageFileNameUtil();
     String twitterFileUserName =
         imageFileNameUtil.getTwitterFileUserName("usbharu_dev-1571732741414334464-img1.png");
-    assertEquals("usbharu_dev",twitterFileUserName);
+    assertEquals("usbharu_dev", twitterFileUserName);
   }
 
   @Test
@@ -27,7 +173,7 @@ class ImageFileNameUtilTest {
     ImageFileNameUtil imageFileNameUtil = new ImageFileNameUtil();
     String twitterFileId =
         imageFileNameUtil.getTwitterFileId("usbharu_dev-1571732741414334464-img1.png");
-    assertEquals("1571732741414334464",twitterFileId);
+    assertEquals("1571732741414334464", twitterFileId);
   }
 
   @Test
@@ -35,7 +181,7 @@ class ImageFileNameUtilTest {
     ImageFileNameUtil imageFileNameUtil = new ImageFileNameUtil();
     String twitterFileNumber =
         imageFileNameUtil.getTwitterFileNumber("usbharu_dev-1571732741414334464-img1.png");
-    assertEquals("1",twitterFileNumber);
+    assertEquals("1", twitterFileNumber);
   }
 
   @Test
@@ -59,7 +205,7 @@ class ImageFileNameUtilTest {
     ImageFileNameUtil imageFileNameUtil = new ImageFileNameUtil();
     String url = "https://twitter.com/usbharu_dev/status/1571732741414334464/photo/1";
     String name = "usbharu_dev-1571732741414334464-img1.png";
-    assertEquals(url,imageFileNameUtil.getTwitterUrl(name));
+    assertEquals(url, imageFileNameUtil.getTwitterUrl(name));
   }
 
   @Test
@@ -67,6 +213,6 @@ class ImageFileNameUtilTest {
     ImageFileNameUtil imageFileNameUtil = new ImageFileNameUtil();
     String url = "https://www.pixiv.net/artworks/123456";
     String name = "123456_p0.png";
-    assertEquals(url,imageFileNameUtil.getPixivUrl(name));
+    assertEquals(url, imageFileNameUtil.getPixivUrl(name));
   }
 }
---
+++ src/test/java/io/github/usbharu/imagesearch/util/ListUtilsTest.java
@@ -0,0 +1,29 @@
+package io.github.usbharu.imagesearch.util;
+
+import static io.github.usbharu.imagesearch.util.ListUtils.getOr;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class ListUtilsTest {
+
+  @Test
+  void getOr_getFromIndex_returnElement() {
+    List<String> list = List.of("a","b","c","d");
+    String actual = getOr(list, 2, "a");
+    String expected = "c";
+    assertEquals(expected,actual);
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = {-1,100,3892843})
+  void getOr_getFromIllegalIndex_returnDefault(int n) {
+    List<String> list = List.of("a","b","c","d");
+    String actual = getOr(list, n, "a");
+    String expected = "a";
+    assertEquals(expected,actual);
+  }
+}

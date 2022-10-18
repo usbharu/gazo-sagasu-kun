c116aa59e5db1ccb431882441040ed6ad5a1fd87
usbharu <64310155+usbharu@users.noreply.github.com>
2022-10-19 08:12:08 +0900

    CsvTagFilterにLoggerを追加

--- src/main/java/io/github/usbharu/imagesearch/domain/service/famous_services/commons/CsvTagFilter.java
+++ src/main/java/io/github/usbharu/imagesearch/domain/service/famous_services/commons/CsvTagFilter.java
@@ -8,50 +8,66 @@ import io.github.usbharu.imagesearch.domain.service.scan.Filter;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UncheckedIOException;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.regex.Pattern;
 import javax.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 import org.supercsv.prefs.CsvPreference;
 
-@Service
-@ConfigurationProperties("aaa.aaa")
+@Component
+@ConfigurationProperties("imagesearch.scan.filter")
 public class CsvTagFilter implements Filter {
 
-  private String path = "";
+  private String path = null;
 
   List<Pattern> patterns = new ArrayList<>();
   List<String> simpleFilter = new ArrayList<>();
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(CsvTagFilter.class);
 
   public CsvTagFilter() {
 
   }
 
+  // TODO: 2022/10/19 一度TagをStringにしてしまって、replaceAllですべて消してしまってからもう一度tagsに戻すほうが早いかもしれない
   @Override
   public ImageMetadata filter(ImageMetadata imageMetadata) {
-    if (imageMetadata instanceof Tags) {
-      Tags metadata = (Tags) imageMetadata;
-      System.out.println("metadata = " + metadata);
-      for (int i = 0, metadataSize = metadata.size(); i < metadataSize; i++) {
-        Tag tag = metadata.get(i);
-        System.out.println("tag.getName() = " + tag.getName());
-        if (simpleFilter.contains(tag.getName())) {
-          System.out.println("tag "+tag.getName()+" is removed!");
-          metadata.set(i, null);
-        } else {
-          for (Pattern pattern : patterns) {
-            if (pattern.matcher(tag.getName()).find()) {
-              metadata.set(i, null);
-            }
+    //フィルタリングの処理は重たいため、フィルターが空の場合はすぐに返す
+    if (!(imageMetadata instanceof Tags) || patterns.size() + simpleFilter.size() == 0) {
+      return imageMetadata;
+    }
+
+    Tags metadata = (Tags) imageMetadata;
+    int size = metadata.size();
+    LOGGER.trace("Filtering : {}", metadata);
+    for (int i = 0, metadataSize = metadata.size(); i < metadataSize; i++) {
+      Tag tag = metadata.get(i);
+
+      if (simpleFilter.contains(tag.getName())) {
+        LOGGER.trace("Removed Tag by plane filter : {}", tag.getName());
+        metadata.set(i, null);
+      } else {
+
+        for (Pattern pattern : patterns) {
+          if (pattern.matcher(tag.getName()).find()) {
+            LOGGER.trace("Removed Tag by regex filter : {}", tag.getName());
+            metadata.set(i, null);
           }
         }
+
       }
-      return metadata;
     }
-    return imageMetadata;
+    metadata.removeIf(Objects::isNull);
+    LOGGER.debug("Filtered {} (before {}, after {})", size - metadata.size(), size,
+        metadata.size());
+    return metadata;
 
   }
 
@@ -61,6 +77,11 @@ public class CsvTagFilter implements Filter {
 
   @PostConstruct
   public void init() {
+    if (path == null) {
+      LOGGER.info("Filter does not exist.");
+      return;
+    }
+    LOGGER.info("Filter Path : {}", path);
     try (
         CsvAnnotationBeanReader<FilterCsv> csvListReader =
             new CsvAnnotationBeanReader<>(FilterCsv.class,
@@ -68,16 +89,22 @@ public class CsvTagFilter implements Filter {
                 CsvPreference.STANDARD_PREFERENCE)) {
       List<FilterCsv> filterCsvs = csvListReader.readAll();
       for (FilterCsv filterCsv : filterCsvs) {
+
         if (filterCsv.getRegex()) {
+
           patterns.add(Pattern.compile(filterCsv.getString()));
+          LOGGER.debug("Add regex filter : {}", filterCsv.getString());
         } else {
+
           simpleFilter.add(filterCsv.getString());
+          LOGGER.debug("Add plane filter : {}", filterCsv.getString());
         }
+
+        LOGGER.info("{} filters have been set.(plane {}, regex {})", filterCsvs.size(),
+            simpleFilter.size(), patterns.size());
       }
     } catch (IOException e) {
-      throw new RuntimeException(e);
+      throw new UncheckedIOException(e);
     }
-    System.out.println("patterns = " + patterns);
-    System.out.println("simpleFilter = " + simpleFilter);
   }
 }

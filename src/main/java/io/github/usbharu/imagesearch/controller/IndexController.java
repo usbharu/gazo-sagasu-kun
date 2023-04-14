2bc0ef3a9a496060174123d28820bde0148d7c8f
usbharu <64310155+usbharu@users.noreply.github.com>
2023-04-15 01:34:31 +0900

    feat: リサイズ後のフォルダに対応

--- src/main/java/io/github/usbharu/imagesearch/controller/IndexController.java
+++ src/main/java/io/github/usbharu/imagesearch/controller/IndexController.java
@@ -7,9 +7,6 @@ import io.github.usbharu.imagesearch.domain.service.ImageSearch;
 import io.github.usbharu.imagesearch.domain.service.ImageService;
 import io.github.usbharu.imagesearch.domain.service.TagService;
 import io.github.usbharu.imagesearch.util.ImageFileNameUtil;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,135 +21,163 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.Optional;
+
 @Controller
 
 public class IndexController {
 
-  final BuildProperties buildProperties;
-  private final ImageFileNameUtil imageFileNameUtil;
-  private final ImageSearch imageSearch;
-  private final TagService tagService;
-  private final GroupService groupService;
-  private final ImageService imageService;
-  Logger logger = LoggerFactory.getLogger(IndexController.class);
-  @Value("${imagesearch.scan.http.folder:}")
-  private String httpFolder = "";
-  @Value("${imagesearch.scan.folder:}")
-  private String folder = "";
+    final BuildProperties buildProperties;
+    private final ImageFileNameUtil imageFileNameUtil;
+    private final ImageSearch imageSearch;
+    private final TagService tagService;
+    private final GroupService groupService;
+    private final ImageService imageService;
+    Logger logger = LoggerFactory.getLogger(IndexController.class);
+    @Value("${imagesearch.scan.http.folder:}")
+    private String httpFolder = "";
+    @Value("${imagesearch.scan.folder:}")
+    private String folder = "";
 
-  @Autowired
-  public IndexController(ImageSearch imageSearch, TagService tagService, GroupService groupService,
-      ImageService imageService, BuildProperties buildProperties,
-      ImageFileNameUtil imageFileNameUtil) {
-    this.imageSearch = imageSearch;
-    this.tagService = tagService;
-    this.groupService = groupService;
-    this.imageService = imageService;
-    this.buildProperties = buildProperties;
-    this.imageFileNameUtil = imageFileNameUtil;
-  }
-
-  @GetMapping("/")
-  public String index(Model model) {
-    logger.trace("Access to Index");
-    model.addAttribute("message", imageSearch.randomTag().getName());
-    model.addAttribute("groups", groupService.getGroupsAndAll());
-    model.addAttribute("version", buildProperties.getVersion());
-    return "index";
-  }
-
-
-  @edu.umd.cs.findbugs.annotations.SuppressFBWarnings("ICAST_INT_CAST_TO_DOUBLE_PASSED_TO_CEIL")
-  @SuppressWarnings("IntegerDivisionInFloatingPointContext")
-  @GetMapping("/search")
-  public String search(@ModelAttribute("tags") String tags, @ModelAttribute("group") String group,
-      @ModelAttribute("sort") String sort, @ModelAttribute("order") String order,
-      @ModelAttribute("duplicate") String duplicate, @ModelAttribute("limit") String limitStr,
-      @ModelAttribute("page") String pageStr, @ModelAttribute("link") String link,
-      @ModelAttribute("merge") String mergeStr, Model model) {
-    if (duplicate != null && !duplicate.isBlank()) {
-      return "redirect:/image/" + duplicate;
-    }
-    if (link != null && !link.isBlank()) {
-      return "redirect:" + link;
+    @Autowired
+    public IndexController(ImageSearch imageSearch, TagService tagService, GroupService groupService,
+                           ImageService imageService, BuildProperties buildProperties,
+                           ImageFileNameUtil imageFileNameUtil) {
+        this.imageSearch = imageSearch;
+        this.tagService = tagService;
+        this.groupService = groupService;
+        this.imageService = imageService;
+        this.buildProperties = buildProperties;
+        this.imageFileNameUtil = imageFileNameUtil;
     }
 
-    if (!groupService.validation(group)) {
-      group = "";
+    @GetMapping("/")
+    public String index(Model model) {
+        logger.trace("Access to Index");
+        model.addAttribute("message", imageSearch.randomTag().getName());
+        model.addAttribute("groups", groupService.getGroupsAndAll());
+        model.addAttribute("version", buildProperties.getVersion());
+        return "index";
     }
 
-    int limit = 100;
-    if (limitStr != null && !limitStr.isBlank()) {
-      try {
-        limit = Integer.parseInt(limitStr);
-      } catch (NumberFormatException e) {
-        logger.warn("Illegal Page Number", e);
-      }
+
+    @edu.umd.cs.findbugs.annotations.SuppressFBWarnings("ICAST_INT_CAST_TO_DOUBLE_PASSED_TO_CEIL")
+    @SuppressWarnings("IntegerDivisionInFloatingPointContext")
+    @GetMapping("/search")
+    public String search(@ModelAttribute("tags") String tags, @ModelAttribute("group") String group,
+                         @ModelAttribute("sort") String sort, @ModelAttribute("order") String order,
+                         @ModelAttribute("duplicate") String duplicate, @ModelAttribute("limit") String limitStr,
+                         @ModelAttribute("page") String pageStr, @ModelAttribute("link") String link,
+                         @ModelAttribute("merge") String mergeStr, Model model) {
+        if (duplicate != null && !duplicate.isBlank()) {
+            return "redirect:/image/" + duplicate;
+        }
+        if (link != null && !link.isBlank()) {
+            return "redirect:" + link;
+        }
+
+        if (!groupService.validation(group)) {
+            group = "";
+        }
+
+        int limit = 100;
+        if (limitStr != null && !limitStr.isBlank()) {
+            try {
+                limit = Integer.parseInt(limitStr);
+            } catch (NumberFormatException e) {
+                logger.warn("Illegal Page Number", e);
+            }
+        }
+        int page = 0;
+        if (pageStr != null && !pageStr.isBlank()) {
+            try {
+                page = Integer.parseInt(pageStr);
+            } catch (NumberFormatException e) {
+                logger.warn("Illegal Page Number", e);
+            }
+        }
+        boolean merge = false;
+        if (mergeStr != null && !mergeStr.isBlank()) {
+            merge = Boolean.parseBoolean(mergeStr);
+        }
+
+        Images images =
+                imageSearch.search3(tags.split("[; ,]"), group, sort, order, limit, page, merge);
+        model.addAttribute("tagCount", tagService.tagOrderOfMostUsedLimit(20));
+        model.addAttribute("message", tags);
+        model.addAttribute("images", images);
+        model.addAttribute("httpUrl", httpFolder);
+        model.addAttribute("groups", groupService.getGroupsAndAll());
+        model.addAttribute("version", buildProperties.getVersion());
+        model.addAttribute("limit", limit);
+        model.addAttribute("page", page);
+        model.addAttribute("pageCount", ((int) Math.ceil(images.getCount() / limit)));
+        model.addAttribute("count", images.getCount());
+        model.addAttribute("merge", merge);
+        return "search";
     }
-    int page = 0;
-    if (pageStr != null && !pageStr.isBlank()) {
-      try {
-        page = Integer.parseInt(pageStr);
-      } catch (NumberFormatException e) {
-        logger.warn("Illegal Page Number", e);
-      }
-    }
-    boolean merge = false;
-    if (mergeStr != null && !mergeStr.isBlank()) {
-      merge = Boolean.parseBoolean(mergeStr);
+
+    @GetMapping("/image/{id}")
+    public String image(@PathVariable("id") Integer id, Model model) {
+        Image image = imageService.findById(id);
+        model.addAttribute("image", image);
+        model.addAttribute("httpUrl", httpFolder);
+        model.addAttribute("version", buildProperties.getVersion());
+        return "image";
     }
 
-    Images images =
-        imageSearch.search3(tags.split("[; ,]"), group, sort, order, limit, page, merge);
-    model.addAttribute("tagCount", tagService.tagOrderOfMostUsedLimit(20));
-    model.addAttribute("message", tags);
-    model.addAttribute("images", images);
-    model.addAttribute("httpUrl", httpFolder);
-    model.addAttribute("groups", groupService.getGroupsAndAll());
-    model.addAttribute("version", buildProperties.getVersion());
-    model.addAttribute("limit", limit);
-    model.addAttribute("page", page);
-    model.addAttribute("pageCount", ((int) Math.ceil(images.getCount() / limit)));
-    model.addAttribute("count", images.getCount());
-    model.addAttribute("merge", merge);
-    return "search";
-  }
-
-  @GetMapping("/image/{id}")
-  public String image(@PathVariable("id") Integer id, Model model) {
-    Image image = imageService.findById(id);
-    model.addAttribute("image", image);
-    model.addAttribute("httpUrl", httpFolder);
-    model.addAttribute("version", buildProperties.getVersion());
-    return "image";
-  }
-
-  @GetMapping("/image/file")
-  public HttpEntity<byte[]> image(@ModelAttribute("path") String path, Model model) {
-    File file = new File(folder + File.separator + path);
-    byte[] byteImg = null;
-    HttpHeaders httpHeaders = null;
-    try {
-      byteImg = Files.readAllBytes(file.toPath());
-      httpHeaders = new HttpHeaders();
-      if (imageFileNameUtil.isJpg(file.getName())) {
-        httpHeaders.setContentType(MediaType.IMAGE_JPEG);
-      } else if (imageFileNameUtil.isPng(file.getName())) {
-        httpHeaders.setContentLength(byteImg.length);
-      }
-    } catch (IOException e) {
-      return null;
+    @GetMapping("/image/file")
+    public HttpEntity<byte[]> image(@ModelAttribute("path") String path, @ModelAttribute("size") String size, Model model) {
+        File file = new File(folder + File.separator + path);
+        byte[] byteImg = null;
+        HttpHeaders httpHeaders = null;
+        try {
+            File file1 = size != null && size.equals("thumbnail") ? getNoResizeFile(file).orElse(file) : file;
+            byteImg = Files.readAllBytes(file1.toPath());
+            httpHeaders = new HttpHeaders();
+            if (imageFileNameUtil.isJpg(file.getName())) {
+                httpHeaders.setContentType(MediaType.IMAGE_JPEG);
+            } else if (imageFileNameUtil.isPng(file.getName())) {
+                httpHeaders.setContentLength(byteImg.length);
+            }
+        } catch (IOException e) {
+            return null;
+        }
+        return new HttpEntity<>(byteImg, httpHeaders);
     }
-    return new HttpEntity<>(byteImg, httpHeaders);
-  }
 
-  @GetMapping("/redirect")
-  public String redirectTo(@ModelAttribute("url") String url) {
-    return "redirect:" + url;
-  }
+    @GetMapping("/redirect")
+    public String redirectTo(@ModelAttribute("url") String url) {
+        return "redirect:" + url;
+    }
 
-  @GetMapping("/link")
-  public String linkTo(@ModelAttribute("url") String url) {
-    return "redirect:" + url;
-  }
+    @GetMapping("/link")
+    public String linkTo(@ModelAttribute("url") String url) {
+        return "redirect:" + url;
+    }
+
+    private File getNoResizeParentFile(File imageFile) {
+        String parent = imageFile.getParent();
+        return new File(parent.substring(0, parent.length() - "-resize".length()));
+    }
+
+    private Optional<File> getNoResizeFile(File imageFile) {
+        File noResizeParentFile = getNoResizeParentFile(imageFile);
+        File[] files = noResizeParentFile.listFiles();
+        if (files == null) {
+            files = new File[0];
+        }
+        return Arrays.stream(files).filter(f -> {
+            String name = f.getName();
+            try {
+                return imageFile.getName().startsWith(name.substring(0, name.lastIndexOf('.')));
+            } catch (StringIndexOutOfBoundsException e) {
+                return false;
+            }
+        }).findFirst();
+    }
 }
--- src/main/java/io/github/usbharu/imagesearch/domain/service/famous_services/pixiv/PixivBatchDownloaderPluginScanner.java
+++ src/main/java/io/github/usbharu/imagesearch/domain/service/famous_services/pixiv/PixivBatchDownloaderPluginScanner.java
@@ -6,72 +6,100 @@ import io.github.usbharu.imagesearch.domain.model.Tag;
 import io.github.usbharu.imagesearch.domain.model.Tags;
 import io.github.usbharu.imagesearch.domain.service.scan.Scanner;
 import io.github.usbharu.imagesearch.domain.service.scan.impl.DefaultJpegScanner;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.UncheckedIOException;
-import java.nio.file.Path;
-import java.util.List;
-import java.util.Objects;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.UncheckedIOException;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
 @Component
 public class PixivBatchDownloaderPluginScanner extends DefaultJpegScanner implements Scanner {
 
-  Logger logger = LoggerFactory.getLogger(PixivBatchDownloaderPluginScanner.class);
+    Logger logger = LoggerFactory.getLogger(PixivBatchDownloaderPluginScanner.class);
 
-  @Override
-  public boolean isSupported(File file) {
-    if (file == null) {
-      logger.warn("File is Null");
-      return false;
-    }
-    return imageFileNameUtil.isJpg(file.getName()) || file.getName().toUpperCase().endsWith("PNG");
-  }
-
-  @Override
-  public Image getMetadata(File imageFile, Path subpath) {
-    Objects.requireNonNull(imageFile, "ImageFile is Null");
-    Objects.requireNonNull(subpath, "Subpath is Null");
-    if (!imageFile.exists()) {
-      throw new UncheckedIOException("ImageFile is not found", new FileNotFoundException());
-    }
-    if (!imageFile.isFile()) {
-      throw new IllegalArgumentException("ImageFile is not file");
+    @Override
+    public boolean isSupported(File file) {
+        if (file == null) {
+            logger.warn("File is Null");
+            return false;
+        }
+        return imageFileNameUtil.isJpg(file.getName()) || file.getName().toUpperCase().endsWith("PNG");
     }
 
-    Image image = null;
-    if (imageFileNameUtil.isJpg(imageFile.getName())) {
-      image = super.getMetadata(imageFile, subpath);
+    @Override
+    public Image getMetadata(File imageFile, Path subpath) {
+        Objects.requireNonNull(imageFile, "ImageFile is Null");
+        Objects.requireNonNull(subpath, "Subpath is Null");
+        if (!imageFile.exists()) {
+            throw new UncheckedIOException("ImageFile is not found", new FileNotFoundException());
+        }
+        if (!imageFile.isFile()) {
+            throw new IllegalArgumentException("ImageFile is not file");
+        }
+
+
+        imageFile = getNoResizeFile(imageFile).orElse(imageFile);
+
+        Image image = null;
+        if (imageFileNameUtil.isJpg(imageFile.getName())) {
+            image = super.getMetadata(imageFile, subpath);
+        }
+
+        if (image == null) {
+            image = new Image(imageFile.getName(), subpath.toString());
+        }
+
+        if (imageFileNameUtil.isPixivTypeFileName(imageFile.getName())) {
+            image.getMetadata().addAll(getPixivImageMetadata(imageFile));
+            Tags metadata = new Tags();
+            metadata.add(
+                    new Tag("--" + imageFileNameUtil.getPixivTypeFileBaseName(imageFile.getName()) + "--"));
+            metadata.add(new Tag("--Pixiv--"));
+            image.addMetadata(metadata);
+            logger.trace(image.toString());
+        }
+        image.setPath(subpath.toString());
+        return image;
     }
 
-    if (image == null) {
-      image = new Image(imageFile.getName(), subpath.toString());
+    protected List<ImageMetadata> getPixivImageMetadata(File image) {
+        File file = new File(
+                image.getParent() + File.separator + imageFileNameUtil.getPixivTypeFileBaseName(
+                        image.getName()) + "-meta.txt");
+        if (file.exists()) {
+            return PixivMetadataParser.parse(file);
+        }
+        return List.of();
+
     }
 
-    if (imageFileNameUtil.isPixivTypeFileName(imageFile.getName())) {
-      image.getMetadata().addAll(getPixivImageMetadata(imageFile));
-      Tags metadata = new Tags();
-      metadata.add(
-          new Tag("--" + imageFileNameUtil.getPixivTypeFileBaseName(imageFile.getName()) + "--"));
-      metadata.add(new Tag("--Pixiv--"));
-      image.addMetadata(metadata);
-      logger.trace(image.toString());
+    private File getNoResizeParentFile(File imageFile) {
+        String parent = imageFile.getParent();
+        return new File(parent.substring(0, parent.length() - "-resize".length()));
     }
-    return image;
-  }
 
-  protected List<ImageMetadata> getPixivImageMetadata(File image) {
-    File file = new File(
-        image.getParent() + File.separator + imageFileNameUtil.getPixivTypeFileBaseName(
-            image.getName()) + "-meta.txt");
-    if (file.exists()) {
-      return PixivMetadataParser.parse(file);
+    private Optional<File> getNoResizeFile(File imageFile) {
+        File noResizeParentFile = getNoResizeParentFile(imageFile);
+        File[] files = noResizeParentFile.listFiles();
+        if (files == null) {
+            files = new File[0];
+        }
+        return Arrays.stream(files).filter(f -> {
+            String name = f.getName();
+            try {
+                return imageFile.getName().startsWith(name.substring(0, name.lastIndexOf('.')));
+            }catch (StringIndexOutOfBoundsException e){
+                return false;
+            }
+        }).findFirst();
     }
-    return List.of();
-
-  }
 
 
 }
